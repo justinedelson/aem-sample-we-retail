50bdefb08d24983b42dddf06a334caa74d3936cb
Burkhard Pauli <pauli@adobe.com>
2016-07-28 18:52:17 +0200

    CQ-91402 - Adaption of the we-retail/components/content/articles-list component
    Changes:
    - Removed unused JSUse object
    - changing resourceSuperType to core/wcm/components/list
    - adapting necessary changes for new resourceSuperType

--- core/src/main/java/we/retail/core/Article.java
+++ core/src/main/java/we/retail/core/Article.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2016 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package we.retail.core;
 
 import java.text.SimpleDateFormat;
@@ -26,9 +41,6 @@ import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.tagging.Tag;
 import com.day.cq.tagging.TagManager;
 
-/**
- * Created by Daniel on 03/12/15.
- */
 @Model(adaptables = {Resource.class})
 public class Article {
     
