9881aa3548dcfb0aacd5f95ac026e7b3bac3266a
Markus Haack <mhaack@adobe.com>
2016-11-10 10:58:37 +0100

    CQ-94559 - Add products to smart lists
    only display heart button for logged in users

--- core/src/main/java/we/retail/core/model/ProductModel.java
+++ core/src/main/java/we/retail/core/model/ProductModel.java
@@ -73,6 +73,7 @@ public class ProductModel {
 
     private CommerceService commerceService;
     private ProductItem productItem;
+    private boolean isAnonymous;
 
     @PostConstruct
     private void populateProduct() {
@@ -94,6 +95,9 @@ public class ProductModel {
         } catch (CommerceException e) {
             LOGGER.error("Can't extract product from page", e);
         }
+
+        isAnonymous = resourceResolver.getUserID() == null || resourceResolver.getUserID().equals("anonymous") ? true
+                : false;
     }
 
     public ProductItem getProductItem() {
@@ -116,6 +120,9 @@ public class ProductModel {
         return commerceHandler.getProductTrackingPath();
     }
 
+    public boolean isAnonymous() {
+        return isAnonymous;
+    }
 
     public class ProductItem {
 
