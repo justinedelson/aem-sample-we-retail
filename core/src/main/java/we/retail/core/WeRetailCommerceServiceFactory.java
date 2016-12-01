c036d92ca78ca6eedc21223ce5a3c34c6354d7ff
Markus Haack <mhaack@adobe.com>
2016-12-01 17:44:41 +0100

    CQ-108604 - Display We.Retail products related to the current shopping cart (no RTC needed)
    Implement recommendation for shopping cart
    Unified code of SimilarToCurrentRelationshipsProvider and SimilarToCartRelationshipsProvider
    Clean up of hard coded context paths in SimilarToCurrentRelationshipsProvider

--- core/src/main/java/we/retail/core/WeRetailCommerceServiceFactory.java
+++ core/src/main/java/we/retail/core/WeRetailCommerceServiceFactory.java
@@ -15,14 +15,15 @@
  ******************************************************************************/
 package we.retail.core;
 
-import com.adobe.cq.commerce.api.CommerceService;
-import com.adobe.cq.commerce.api.CommerceServiceFactory;
-import com.adobe.cq.commerce.common.AbstractJcrCommerceServiceFactory;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.Resource;
+
+import com.adobe.cq.commerce.api.CommerceService;
+import com.adobe.cq.commerce.api.CommerceServiceFactory;
+import com.adobe.cq.commerce.common.AbstractJcrCommerceServiceFactory;
 
 /**
  * we.retail implementation for the {@link CommerceServiceFactory} interface.
@@ -31,7 +32,7 @@ import org.apache.sling.api.resource.Resource;
 @Service
 @Properties(value = {
     @Property(name = "service.description", value = "Factory for reference implementation commerce service"),
-    @Property(name = "commerceProvider", value = "we-retail", propertyPrivate = true)
+    @Property(name = "commerceProvider", value = WeRetailConstants.WE_RETAIL_COMMERCEPROVIDER, propertyPrivate = true)
 })
 public class WeRetailCommerceServiceFactory  extends AbstractJcrCommerceServiceFactory implements CommerceServiceFactory {
     /**
