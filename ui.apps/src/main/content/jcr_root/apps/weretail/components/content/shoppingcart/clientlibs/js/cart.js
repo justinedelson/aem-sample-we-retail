f7e4ea5c66e74df37599776d985ae38953bc5e76
Markus Haack <mhaack@adobe.com>
2016-12-13 15:35:59 +0100

    CQ-110010 - We.Retail cart is not in sync with ContextHub cart
    improve JS

--- ui.apps/src/main/content/jcr_root/apps/weretail/components/content/shoppingcart/clientlibs/js/cart.js
+++ ui.apps/src/main/content/jcr_root/apps/weretail/components/content/shoppingcart/clientlibs/js/cart.js
@@ -16,10 +16,6 @@
 (function ($) {
     'use strict';
 
-    if ($('div.we-ShoppingCart-empty').length > 0) {
-        $('a.btn-primary').hide();
-    }
-
     var bindQuantityButtons = function () {
         $('input[name="quantity"]').change(function () {
             $(this).parents('form').submit();
@@ -27,38 +23,34 @@
     }
 
     var bindCartForms = function () {
-        $('.we-ShoppingCart form, .we-Cart-content form, .we-Product-form').submit(function (event) {
-            event.preventDefault();
-            var $form = $(event.target);
-            $.ajax({
-                url: $form.attr('action'),
-                data: $form.serialize(),
-                cache: false,
-                type: 'POST',
-                success: function (json) {
-                    // $('.we-ShoppingCart').replaceWith(json.shoppingCart);
-                    if (window.ContextHub) {
-                        ContextHub.getStore("cart").queryService();
-                    } else {
-                        refreshCart();
-                    }
-                    //$('.we-Cart').replaceWith(json.navCart);
+        $('.we-ShoppingCart form, .we-Cart-content form, .we-Product-form').submit(submitForm);
+    }
 
-                    // This reinitializes the navcart Vue.js component
-                    // See /apps/weretail/components/structure/navcart/clientlibs/js/cart.js
-                    //navcart.call(this);
+    var toggleCartEmptyMsg = function () {
+        if ($('div.we-ShoppingCart div.we-ShoppingCart-empty').length > 0) {
+            $('a.btn-primary').hide();
+        } else {
+            $('a.btn-primary.btn-cart').show();
+        }
+    }
 
-                    if (json.entries == '0') {
-                        $('.shoppingcart-prices').remove();
-                    }
-                    else {
-                        $('.shoppingcart-prices').replaceWith(json.cartPrices);
-                    }
-                },
-                error: function () {
-                    alert('An error occured while trying to perform this operation.');
-                }
-            });
+    var submitForm = function (event) {
+        event.preventDefault();
+        var $form = $(event.target);
+        $.ajax({
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            cache: false,
+            type: 'POST'
+        }).done(function (json) {
+            if (window.ContextHub) {
+                ContextHub.getStore("cart").queryService();
+            } else {
+                refreshCart();
+            }
+
+        }).fail(function () {
+            alert('An error occured while trying to perform this operation.');
         });
     }
 
@@ -67,16 +59,13 @@
         shoppingCart.parent().load(Granite.HTTP.externalize(shoppingCart.data("resource") + ".html"), function () {
             bindCartForms();
             bindQuantityButtons();
-            if ($('div.we-ShoppingCart div.we-ShoppingCart-empty').length > 0) {
-                $('a.btn-primary').hide();
-            } else {
-                $('a.btn-primary').show();
-            }
+            toggleCartEmptyMsg();
         });
     }
 
     bindCartForms();
     bindQuantityButtons();
+    toggleCartEmptyMsg();
 
     if (window.ContextHub) {
         ContextHub.eventing.on(ContextHub.Constants.EVENT_STORE_UPDATED + ":cart", refreshCart);
