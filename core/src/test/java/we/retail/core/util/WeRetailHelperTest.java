7273dad35fa12daf975c16cee410256f0c917b2c
Christophe Jelger <jelger@adobe.com>
2017-01-26 14:50:42 +0100

    CQ-4196591 - We.Retail Commerce test coverage
    - Added more unit tests

--- core/src/test/java/we/retail/core/util/WeRetailHelperTest.java
+++ core/src/test/java/we/retail/core/util/WeRetailHelperTest.java
@@ -16,6 +16,7 @@
 package we.retail.core.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -28,7 +29,6 @@ import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 
 import com.day.cq.wcm.api.NameConstants;
 import com.day.cq.wcm.api.Page;
@@ -40,19 +40,17 @@ import we.retail.core.model.Constants;
 public class WeRetailHelperTest {
 
     private static final String MOCK_RESOURCE_TITLE = "mockResourceTitle";
+    private static final String ORDER_DETAILS_TITLE = "Order Details";
 
     @Rule
     public final AemContext context = AppAemContext.newAemContext();
 
     private Page page;
 
-    @Mock
-    private Resource resource;
-
     @Before
     public void setUp() throws Exception {
         page = context.currentPage(Constants.TEST_ORDER_PAGE);
-        resource = context.currentResource(Constants.TEST_ORDER_RESOURCE);
+        context.currentResource(Constants.TEST_ORDER_RESOURCE);
     }
 
     @Test
@@ -62,8 +60,19 @@ public class WeRetailHelperTest {
         map.put(NameConstants.PN_TITLE, MOCK_RESOURCE_TITLE);
         ValueMap vm = new ValueMapDecorator(map);
         when(mockResource.adaptTo(ValueMap.class)).thenReturn(vm);
+
         assertEquals(MOCK_RESOURCE_TITLE, WeRetailHelper.getTitle(mockResource, page));
-        assertEquals("Order Details", WeRetailHelper.getPageTitle(page));
-        assertEquals("Order Details", WeRetailHelper.getTitle(page));
+        assertEquals(ORDER_DETAILS_TITLE, WeRetailHelper.getPageTitle(page));
+        assertEquals(ORDER_DETAILS_TITLE, WeRetailHelper.getTitle(page));
+
+        assertNull(WeRetailHelper.getTitle(null, page));
+        assertNull(WeRetailHelper.getPageTitle(null));
+        assertNull(WeRetailHelper.getTitle(null));
+
+        // If the resource does not have a title, the page title is returned
+        Map<String, Object> map2 = new HashMap<String, Object>();
+        ValueMap vm2 = new ValueMapDecorator(map2);
+        when(mockResource.adaptTo(ValueMap.class)).thenReturn(vm2);
+        assertEquals(ORDER_DETAILS_TITLE, WeRetailHelper.getTitle(mockResource, page));
     }
 }
