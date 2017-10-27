9654019aa9b5715d9aae0409629a6ecf9bf98d0d
Vivek Singh <vivesing@adobe.com>
2017-10-27 19:11:09 +0530

    CQ-4208122: [We.Retail] Stop exposing We.Retail's implementation classes

--- core/src/test/java/common/AppAemContext.java
+++ core/src/test/java/common/AppAemContext.java
@@ -20,21 +20,27 @@ import java.io.IOException;
 
 import javax.annotation.Nullable;
 
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 
 import com.adobe.cq.commerce.api.CommerceService;
 import com.adobe.cq.commerce.api.Product;
+import com.adobe.cq.social.community.api.CommunityContext;
+import com.day.cq.wcm.api.Page;
 import com.google.common.base.Function;
 
 import common.mock.MockCommerceService;
+import common.mock.MockCommunityContext;
 import common.mock.MockProduct;
+import common.mock.MockUserManager;
 import io.wcm.testing.mock.aem.junit.AemContext;
 import io.wcm.testing.mock.aem.junit.AemContextCallback;
 
 public class AppAemContext {
 
-    public static final String CONTENT_ROOT = "/content/we-retail/us/en";
+    public static final String CONTENT_ROOT = "/content/we-retail";
     public static final String PRODUCT_ROOT = "/var/commerce/products/we-retail";
     public static final String ORDER_ROOT = "/var/commerce/orders/2016/12/12/order";
 
@@ -68,6 +74,20 @@ public class AppAemContext {
                 @Override
                 public Product apply(@Nullable Resource resource) {
                     return new MockProduct(resource);
+                }
+            });
+            context.registerAdapter(Page.class, CommunityContext.class, new Function<Page, CommunityContext>() {
+                @Nullable
+                @Override
+                public CommunityContext apply(@Nullable Page page) {
+                    return new MockCommunityContext();
+                }
+            });
+            context.registerAdapter(ResourceResolver.class, UserManager.class, new Function<ResourceResolver, UserManager>() {
+                @Nullable
+                @Override
+                public UserManager apply(@Nullable ResourceResolver resolver) {
+                    return new MockUserManager();
                 }
             });
             context.addModelsForPackage("we.retail.core.model");
--- core/src/test/java/we/retail/core/model/Constants.java
+++ core/src/test/java/we/retail/core/model/Constants.java
@@ -55,4 +55,22 @@ public class Constants {
 
     public static final String ORDER_ID = "orderId";
     public static final String ORDER_PLACED = "orderPlaced";
+    
+	public static final String TEST_HOME_PAGE = "/content/we-retail/us/en";
+
+	public static final String HEADER_SIGNIN_PATH = "/content/we-retail/us/en/community/signin";
+	public static final String HEADER_SIGNUP_PATH = "/content/we-retail/us/en/community/signup";
+	public static final String HEADER_FORGOT_PWD_PATH = "/content/we-retail/us/en/community/useraccount/forgotpassword";
+	public static final String HEADER_MESSAGING_PATH = "/content/we-retail/us/en/community/messaging";
+	public static final String HEADER_NOTIFICATION_PATH = "/content/we-retail/us/en/community/notifications";
+	public static final String HEADER_MODERATION_PATH = "/content/we-retail/us/en/community/moderation";
+	public static final String HEADER_PROFILE_PATH = "/content/we-retail/us/en/community/profile";
+	public static final String HEADER_ACCOUNT_PATH = "/content/we-retail/us/en/user/account";
+	public static final String HEADER_THEME = "inverse";
+	public static final String HEADER_LANG_ROOT = "/content/we-retail/us/en.html";
+	public static final String HEADER_CURR_LANG = "English";
+	public static final int HEADER_COUNTRY_SIZE = 7;
+	public static final int HEADER_ITEM_SIZE = 6;
+
+	public static final int FOOTER_ITEM_SIZE = 6;
 }
